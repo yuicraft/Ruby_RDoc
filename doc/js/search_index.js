68917f9da8df7f0e2eb19f9ee7bac4e509a66a1c
yuicraft <twins20131988@gmail.com>
2018-10-11 01:55:30 +0900

    루비 변수 설명

---
+++ doc/js/search_index.js
@@ -0,0 +1,1 @@
+var search_data = {"index":{"searchIndex":["variable"],"longSearchIndex":["variable"],"info":[["Variable","","Variable.html","","<p>루비 변수\n<p>Author &mdash; yuicraft\n<p>Version &mdash; 0.0.1\n"]]}}
